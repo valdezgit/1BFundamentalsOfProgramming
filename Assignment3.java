82d285d7243a56dbf0ba6e4aa19505a6a244c9f3
valdezgit <valblvbv@gmail.com>
2026-09-03 01:49:06 +0800

    BSIT

---
+++ Assignment3.java
@@ -0,0 +1,23 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    }
+
+public class Main {
+    public static void main(String[]args){
+        String firstName = "Bianca Louise";
+        String lastName = "Valdez";
+        String address = "Pasileng Sur, Binalonan, Pangasinan";
+        String mobileNumber = "09618234174";
+        int age = 18;
+        double gwa = 95;
+
+        System.out.println("Firstname: " + firstName);
+        System.out.println("Lastname: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("Mobile Number: " + mobileNumber);
+        System.out.println("Age: " + age);
+        System.out.println("GWA: " + gwa);
+    }
+}
